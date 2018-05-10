686aaefdce604f2d98d0c629a6a3aa8a89cb93b4
David <orozco.david101@gmail.com>
2018-05-10 00:42:55 -0700

    more stuff

---
+++ face.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  2 01:55:09 2018
+
+@author: david
+"""
+
+import numpy as np
+import cv2 as cv
+
+face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
+img = cv.imread('faces.jpg')
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+for (x,y,w,h) in faces:
+    cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    for (ex,ey,ew,eh) in eyes:
+        cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+cv.imshow('img',img)
+cv.waitKey(0)
+cv.destroyAllWindows()
